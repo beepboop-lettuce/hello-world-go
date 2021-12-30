f123f88bb16d0d6c224f61f20c29b0c54b6125f1
beepboop-lettuce <ericaharris0486@gmail.com>
2021-12-30 09:55:21 -0600

    added reader to main func created userInput to collect a string from user added Println to print user input

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
 	whatToSay := doctor.Intro()
 
 	fmt.Println(whatToSay)
+
+	userInput, _ := reader.ReadString('\n')
+
+	fmt.Println(userInput)
 }
